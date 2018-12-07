0751409a74fbc7985e50ad970e173726d763ca90
Vaishalik07 <33698785+Vaishalik07@users.noreply.github.com>
2018-12-06 17:42:51 -0800

    Update model.go

--- src/github.com/vkoul07/restapi/models/model.go
+++ src/github.com/vkoul07/restapi/models/model.go
@@ -3,13 +3,10 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type CardHolder struct {
-    ID        bson.ObjectId  `bson:"_id",json:"id"`
+    Id        bson.ObjectId  `bson:"_id",json:"id"`
     FirstName string         `bson:"firstname",json:"firstname"`
     LastName  string         `bson:"lastname",json:"lastname"`
-    Address  *Address        `bson:"address",json:"address"`
-}
-
-type Address struct {
-  City string   `bson:"city",json:"city"`
-  State string  `bson:"state",json:"state"`
+    Address   string         `bson:"address",json:"address"`
+    cardinfo                 `bson:"address",json:"address"`
+    balance                  `bson:"address",json:"address"`
 }
